d07bee1020aade8dd3ab8239c2e7884341e52f86
Mehmet Hassu <hassu.mehmet@gmail.com>
2023-09-05 14:41:41 +0300

    ahhhhhhhhhhhh
    ahhhhhhhhhhhhhhhhhhh

--- Assets/Scripts/GridInst.cs
+++ Assets/Scripts/GridInst.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridInst : MonoBehaviour
 {
     [SerializeField] GameObject gridPrefab;
     [SerializeField] float number;
-
+    public TMP_FontAsset font;
+    
     void Start()
     {
         for (int i = 0; i < number; i++)
@@ -14,7 +17,16 @@ public class GridInst : MonoBehaviour
             for (int y = 0; y < number; y++)
             {
                 GameObject grid = Instantiate(gridPrefab) as GameObject;
-                grid.transform.position = new Vector3(i, y, 0f);
+                grid.transform.position = new Vector3((i-8), (y-8), 0f);
+                GameObject textObject = new GameObject("Text");
+                textObject.transform.position = new Vector3(((float)i - 8), ((float)y - 8.15f), 0f);
+                // TextMeshPro bileþenini ekleyin ve metni ayarlayýn
+                TextMeshPro textMeshPro = textObject.AddComponent<TextMeshPro>();
+                textMeshPro.text = "*"; // Metin içeriðini belirlediðiniz diziye göre alýn
+                textMeshPro.color = Color.white;
+                textMeshPro.fontSize = 9; // Metin boyutunu ayarlayabilirsiniz
+                textMeshPro.alignment = TextAlignmentOptions.Center; // Metin hizalamasýný ayarlayabilirsiniz
+                textMeshPro.font = font; // Kullanmak istediðiniz fontu ayarlayýn
             }
         }
     }
